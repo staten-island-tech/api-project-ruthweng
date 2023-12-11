5e5423169ebce9d71fd44d0343efa69bfbcec94b
ruthweng <ruthweng333@gmail.com>
2023-12-11 13:03:23 -0500

    6

--- API-Burger/api.js
+++ API-Burger/api.js
@@ -1,3 +1,10 @@
 async function getData() {
-    
-}
+    let res =await fetch (
+    "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+    );
+
+let data = await res.json();
+console.log(data);
+//data.results.forEach((object)) => console.log((object));
+    }
+getData();
---
+++ API-Burger/index.js
@@ -0,0 +1,5 @@
+console.log("start");
+setTimeout(() => {
+    console.log("Timer");
+}, 3000);
+console.log("end");
